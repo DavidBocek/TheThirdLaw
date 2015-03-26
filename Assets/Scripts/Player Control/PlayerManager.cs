2a3325043ac926db1d58d6613b4459903a232cf5
ddperera <ddperera@mit.edu>
2015-03-26 01:08:04 -0400

    Added joystick movement, toggle on or off in player prefabs

--- Assets/Scripts/Player Control/PlayerManager.cs
+++ Assets/Scripts/Player Control/PlayerManager.cs
@@ -5,14 +5,37 @@ public class PlayerManager : MonoBehaviour {
 
 	//identifier
 	public int playerNumber = 0;
+	public bool useJoystick = false;
 
 	//indexing
 	private int playerIndex = -1;
 
 	// input abstraction
 	public bool Fire1 { get { return Input.GetButtonDown("Player"+playerIndex+"-Fire1");}}
-	public float HorizontalAxis { get { return Input.GetAxis("Player"+playerIndex+"-Horizontal");}}
-	public float VerticalAxis { get { return Input.GetAxis("Player"+playerIndex+"-Vertical");}}
+	public float HorizontalAxis { 
+		get { 
+			if(useJoystick)
+			{
+				return Input.GetAxis("Player"+playerIndex+"-HorizontalJoy");
+			}
+			else
+			{
+				return Input.GetAxis("Player"+playerIndex+"-Horizontal");
+			}
+		}
+	}
+	public float VerticalAxis { 
+		get {
+			if(useJoystick)
+			{
+				return Input.GetAxis("Player"+playerIndex+"-VerticalJoy");
+			}
+			else
+			{
+				return Input.GetAxis("Player"+playerIndex+"-Vertical");
+			}
+		}
+	}
 
 	//reference abstraction
 	public GameObject PlayerObj {get; set;}
