a843c06a95444466c91544b19abaa23c592409f6
DavidBocek <dmbocek@gmail.com>
2015-04-07 16:34:44 -0400

    adjustments for screen shake and put on player death hook

--- Assets/Scripts/Objects and Interactables/ObjectShake.cs
+++ Assets/Scripts/Objects and Interactables/ObjectShake.cs
@@ -6,6 +6,8 @@ public class ObjectShake : MonoBehaviour {
 	private Vector3 startingPoint;
 	private bool isShaking = false;
 	public float shakeTime = .05f;
+	public float shakeAmount = 1f;
+	public int totalPoints = 3;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +16,9 @@ public class ObjectShake : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)){
+			StartShake();
+		}
 	}
 
 	public void StartShake()
@@ -29,7 +34,7 @@ public class ObjectShake : MonoBehaviour {
 		isShaking = true;
 		Vector3[] shakePoints = ShakePoints ();
 		Vector3 lerpVec = new Vector3();
-		for(int pointNum = 0; pointNum<3; pointNum++)
+		for(int pointNum = 0; pointNum<totalPoints; pointNum++)
 		{
 			for(float t = 0f; t<1f; t+=Time.deltaTime/shakeTime)
 			{
@@ -50,15 +55,16 @@ public class ObjectShake : MonoBehaviour {
 
 	private Vector3[] ShakePoints()
 	{
-		float[] randomAngles = new float[3];
-		for(int i=0; i<3; i++)
+		float[] randomAngles = new float[totalPoints];
+		for(int i=0; i<totalPoints; i++)
 		{
 			randomAngles[i] = Random.Range(0, 2*Mathf.PI);
 		}
-		Vector3[] points = new Vector3[3];
-		for(int i=0; i<3; i++)
+		Vector3[] points = new Vector3[totalPoints];
+		for(int i=0; i<totalPoints; i++)
 		{
 			points[i] = new Vector3(Mathf.Cos(randomAngles[i]), Mathf.Sin (randomAngles[i]), startingPoint.z);
+			points[i] *= shakeAmount / (Mathf.Pow(2,i));
 		}
 
 		return points;
