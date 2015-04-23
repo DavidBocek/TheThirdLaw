22f550d44b301ea9ad07c012f2cd2d02d080af37
ddperera <ddperera@mit.edu>
2015-04-23 13:54:52 -0400

    trying to pull David's changes

--- Assets/Scripts/Objects and Interactables/Asteroid.cs
+++ Assets/Scripts/Objects and Interactables/Asteroid.cs
@@ -5,24 +5,28 @@ public class Asteroid : MonoBehaviour {
 
 	private Vector3 vel;
 	private Transform pictureTrans;
+	private float myRollAngle;
 
 	void FixedUpdate()
 	{
 		transform.Translate(Time.fixedDeltaTime * vel);
+		pictureTrans.Rotate(Vector3.forward, myRollAngle);
 	}
 
 	public void InitializeMotion(Vector2 motion)
 	{
 		vel = new Vector3(motion.x, motion.y);
-		pictureTrans = GetComponentInChildren<Transform>();
-		pictureTrans.Rotate(Vector3.forward, Random.Range(360f));
+		pictureTrans = GetComponentInChildren<SpriteRenderer>().transform;
+		pictureTrans.Rotate(Vector3.forward, Random.Range(0f, 360f));
+
+		myRollAngle = Random.Range (0,2) == 0 ? Random.Range(-1f, -.5f) : Random.Range (.5f, 1f);
 	}
 
 	void OnTriggerEnter2D(Collider2D coll)
 	{
-		if (coll.GetComponent<Collider>().gameObject.CompareTag("Player"))
+		if (coll.gameObject.CompareTag("Player"))
 		{
-			coll.GetComponent<Collider>().GetComponent<Health>().OnImpact();
+			coll.GetComponent<Health>().OnImpact();
 		}
 	}
 
